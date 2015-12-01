11ea0319457cf96e18397a6164021e413cedea68
George II <georgevassiliadis@hotmail.com>
2015-12-01 20:32:12 +0100

    client side updated to new class diagram

---
+++ Application/TriviaClient/TriviaClient/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient
+{
+    class Game
+    {
+        public int id;
+
+        /// <summary>
+        /// It sets the unique id on the client.
+        /// </summary>
+        public void setId();
+
+        /// <summary>
+        /// It notifies the client that the game starts.
+        /// </summary>
+        void startGame();
+
+        /// <summary>
+        /// It receives a message from the chat.
+        /// </summary>
+        /// <param name="id">The id of the player that sent the message.</param>
+        /// <param name="message">The body of the message.</param>
+        void getMessage(int id, string message);
+    }
+}
