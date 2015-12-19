e49487367f147936efa318533bcfdfe32a1828a0
Todor Tsekov <tunchev17@gmail.com>
2015-12-19 13:03:42 +0100

    Created a simple app
    Fixed this annoying error: 'Microsoft.ACE.OLEDB.12.0' provider is not
    registered on the local machine, now I will continue with the rest.

---
+++ Application/Todor's Baby steps/SimpleDatabaseApplication/SimpleDatabaseApplication/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SimpleDatabaseApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_checkConnection = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_checkConnection
+            // 
+            this.lb_checkConnection.AutoSize = true;
+            this.lb_checkConnection.Location = new System.Drawing.Point(47, 52);
+            this.lb_checkConnection.Name = "lb_checkConnection";
+            this.lb_checkConnection.Size = new System.Drawing.Size(0, 13);
+            this.lb_checkConnection.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lb_checkConnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_checkConnection;
+    }
+}
+
---
+++ Application/Todor's Baby steps/SimpleDatabaseApplication/SimpleDatabaseApplication/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace SimpleDatabaseApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try {
+                OleDbConnection connection;
+
+        // This is for access to an ACCESS datadase
+        String providerInfo = "Provider=Microsoft.ACE.OLEDB.12.0"; //for a accdb-database.
+
+                String applicationPath = Application.ExecutablePath.Substring(0, Application.ExecutablePath.LastIndexOf("\\"));
+                String databaseInfo = "Data Source=" + applicationPath + "/TriviaDatabase.accdb";
+
+                //And if you use "|DataDirectory|":
+                // path = "Data Source=|DataDirectory|/StudentsDBS.accdb";
+
+                String connectionInfo = providerInfo + ";" + databaseInfo;
+                connection = new OleDbConnection(connectionInfo);
+
+
+
+
+//                OleDbConnection connection = new OleDbConnection();
+//                connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\todor\Documents\GitHub\Middleware\Application\Todor's Baby steps\TriviaDatabase.accdb;
+//Persist Security Info=False;";
+                connection.Open();
+                lb_checkConnection.Text = "Connection Successful";
+                connection.Close();
+            }
+            catch(Exception dbex)
+            {
+                MessageBox.Show("We are sorry.You cannot connect to the database."+dbex);
+            }
+        }
+    }
+}
