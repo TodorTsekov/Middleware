078f20cca9739dd227959d9a900cf4a5afc5cd2a
Todor Tsekov <tunchev17@gmail.com>
2015-12-19 14:52:17 +0100

    Login without WCF works
    Now I need to do the register

--- Application/Todor's Baby steps/SimpleDatabaseApplication/SimpleDatabaseApplication/Form1.Designer.cs
+++ Application/Todor's Baby steps/SimpleDatabaseApplication/SimpleDatabaseApplication/Form1.Designer.cs
@@ -29,25 +29,95 @@
         private void InitializeComponent()
         {
             this.lb_checkConnection = new System.Windows.Forms.Label();
+            this.grpbx_loging = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.but_login = new System.Windows.Forms.Button();
+            this.grpbx_loging.SuspendLayout();
             this.SuspendLayout();
             // 
             // lb_checkConnection
             // 
             this.lb_checkConnection.AutoSize = true;
-            this.lb_checkConnection.Location = new System.Drawing.Point(47, 52);
+            this.lb_checkConnection.Location = new System.Drawing.Point(12, 9);
             this.lb_checkConnection.Name = "lb_checkConnection";
             this.lb_checkConnection.Size = new System.Drawing.Size(0, 13);
             this.lb_checkConnection.TabIndex = 0;
+            // 
+            // grpbx_loging
+            // 
+            this.grpbx_loging.Controls.Add(this.label2);
+            this.grpbx_loging.Controls.Add(this.label1);
+            this.grpbx_loging.Controls.Add(this.tb_password);
+            this.grpbx_loging.Controls.Add(this.tb_username);
+            this.grpbx_loging.Controls.Add(this.but_login);
+            this.grpbx_loging.Location = new System.Drawing.Point(12, 45);
+            this.grpbx_loging.Name = "grpbx_loging";
+            this.grpbx_loging.Size = new System.Drawing.Size(268, 191);
+            this.grpbx_loging.TabIndex = 1;
+            this.grpbx_loging.TabStop = false;
+            this.grpbx_loging.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(104, 106);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(135, 20);
+            this.tb_password.TabIndex = 2;
+            this.tb_password.UseSystemPasswordChar = true;
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(104, 50);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(135, 20);
+            this.tb_username.TabIndex = 1;
+            // 
+            // but_login
+            // 
+            this.but_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but_login.Location = new System.Drawing.Point(78, 150);
+            this.but_login.Name = "but_login";
+            this.but_login.Size = new System.Drawing.Size(104, 35);
+            this.but_login.TabIndex = 0;
+            this.but_login.Text = "Login";
+            this.but_login.UseVisualStyleBackColor = true;
+            this.but_login.Click += new System.EventHandler(this.but_login_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.grpbx_loging);
             this.Controls.Add(this.lb_checkConnection);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpbx_loging.ResumeLayout(false);
+            this.grpbx_loging.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +126,12 @@
         #endregion
 
         private System.Windows.Forms.Label lb_checkConnection;
+        private System.Windows.Forms.GroupBox grpbx_loging;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.Button but_login;
     }
 }
 
--- Application/Todor's Baby steps/SimpleDatabaseApplication/SimpleDatabaseApplication/Form1.cs
+++ Application/Todor's Baby steps/SimpleDatabaseApplication/SimpleDatabaseApplication/Form1.cs
@@ -12,33 +12,25 @@ using System.Data.OleDb;
 namespace SimpleDatabaseApplication
 {
     public partial class Form1 : Form
-    {
+    { 
+        private OleDbConnection connection;
         public Form1()
         {
             InitializeComponent();
-           
+            // This is for access to an ACCESS datadase
+            String providerInfo = "Provider=Microsoft.ACE.OLEDB.12.0"; //for a accdb-database.
+
+            String applicationPath = Application.ExecutablePath.Substring(0, Application.ExecutablePath.LastIndexOf("\\"));
+            String databaseInfo = "Data Source=" + applicationPath + "/TriviaDatabase.accdb";
+
+            String connectionInfo = providerInfo + ";" + databaseInfo;
+            connection = new OleDbConnection(connectionInfo);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             try {
-                OleDbConnection connection;
-
-        // This is for access to an ACCESS datadase
-        String providerInfo = "Provider=Microsoft.ACE.OLEDB.12.0"; //for a accdb-database.
-
-                String applicationPath = Application.ExecutablePath.Substring(0, Application.ExecutablePath.LastIndexOf("\\"));
-                String databaseInfo = "Data Source=" + applicationPath + "/TriviaDatabase.accdb";
-
-                //And if you use "|DataDirectory|":
-                // path = "Data Source=|DataDirectory|/StudentsDBS.accdb";
-
-                String connectionInfo = providerInfo + ";" + databaseInfo;
-                connection = new OleDbConnection(connectionInfo);
-
-
-
-
+          
 //                OleDbConnection connection = new OleDbConnection();
 //                connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\todor\Documents\GitHub\Middleware\Application\Todor's Baby steps\TriviaDatabase.accdb;
 //Persist Security Info=False;";
@@ -50,6 +42,33 @@ namespace SimpleDatabaseApplication
             {
                 MessageBox.Show("We are sorry.You cannot connect to the database."+dbex);
             }
+        }
+
+        private void but_login_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection;
+            command.CommandText = "select * from user_info where username='"+tb_username.Text+"' and password='"+tb_password.Text+"'";
+            OleDbDataReader reader= command.ExecuteReader();
+            int count = 0;
+            while (reader.Read())
+            {
+                count++;
+            }
+            if (count==1)
+            {
+                MessageBox.Show("Username and password are correct.");
+            }
+            else if (count > 1)
+            {
+                MessageBox.Show("The Username and password are existing twice.");
+            }
+           else
+            {
+                MessageBox.Show("Username and password are not correct.Or there is no such user in our system.");
+            }
+            connection.Close();
         }
     }
 }
