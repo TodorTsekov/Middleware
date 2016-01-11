176de038da63c4acc673f0e43e77cb012ebf2429
TrinaLin <gesasalin@gmail.com>
2016-01-11 11:35:55 +0100

    client

---
+++ Application/lin/chat/client/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string Name { get; set; }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox1.Text))
+            {
+                this.Name = textBox1.Text;
+                this.Close();
+            }
+            else
+                MessageBox.Show("please enter a name", "erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Name = String.Empty;
+            this.Close();
+        }
+    }
+}
