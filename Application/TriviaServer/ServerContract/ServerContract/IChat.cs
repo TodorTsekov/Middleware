c93594988d9255b85c4c07649e8871968daff4f6
George II <georgevassiliadis@hotmail.com>
2016-01-27 12:07:59 +0100

    commit message

---
+++ Application/TriviaServer/ServerContract/ServerContract/IChat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace TriviaContract
+{
+    public interface IChat
+    {
+        /// <summary>
+        /// It sends a player's message to the server.
+        /// </summary>
+        /// <param name="player_id">The player that send the message.</param>
+        /// <param name="message">The message that was sent.</param>
+        void sendMessage(int player_id, string message);
+    }
+}
---
+++ Application/TriviaServer/ServerContract/ServerContract/Utilities/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaContract
+{
+    /// <summary>
+    /// It declares a question and its possible answers.
+    /// </summary>
+    [Serializable]
+    public class Question
+    {
+        public int id;
+        public string questionText;
+        public Answer answer;
+
+        public Question()
+        {
+
+        }
+
+        public Question(int id, string text, Answer answer)
+        {
+            this.id = id;
+            this.questionText = text;
+            this.answer = answer;
+        }
+    }
+}
